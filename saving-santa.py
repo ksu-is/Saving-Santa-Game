d313358a3dd142443c38991d2e87d40e23798597
AlaysiahHernton <142437051+AlaysiahHernton@users.noreply.github.com>
2023-11-20 18:13:01 -0500

    added timing between sentences for suspense

--- saving-santa.py
+++ saving-santa.py
@@ -1,32 +1,55 @@
+import time
+
 print("It's the night before chrismas and all through the North Pole")
+time.sleep(2)
+
 print("the elves are hard at work, busy in Santa's workshop.")
+time.sleep(2)
+
 print("Stockings are hung by chimneys with care,")
+time.sleep(2)
+
 print("as the elves wrap the final presents and load up Santa's sleigh.\n")
+time.sleep(3)
 
 print("Suddenly, a loud crash echoes through the silent night!")
+time.sleep(3)
+
 print("The elves gasp as the workshop door flies open with a bang, ")
 print("a blast of cold air rushing in.")
+time.sleep(2)
+
 print("Standing in the doorway is the shadowy figure of Krampus, ")
 print("the evil christmas monster!")
+time.sleep(3)
+
 print("His eyes glow red as he lets out a menacing laugh. \n")
+time.sleep(3)
 
 print('"Ho ho ho!" mocks Krampus. "You didn\'t think Christmas would go on without a hitch, did you?"\n')
+time.sleep(3)
 
 print("With a snap of his sinister fingers, Krampus traps the elves in an icy cage.")
 print("Santa steps forward, apalled.\n")
+time.sleep(4)
 
 print('"Krampus! What is the meaning of this?" Santa demands.\n')
+time.sleep(2)
 
 print('"Isn\'t it obvious, Santa? I\'m, taking over Christmas this year!')
 print('And you won\'t be able to stop me," Krampus sneers.\n')
+time.sleep(3)
 
 print("He grabs Santa in his clawed hand and throws a magical sack over his head")
 print('Santa cries out, struggling against Krampus\'s grasp.\n')
+time.sleep(4)
 
 print('"Somebody save me!" Santa calls as Krampus drags him out into the darkness.\n')
+time.sleep(2)
 
 print("The elves look around in dismay. Christmas is doomed unless someone can stop")
 print("Krampus and save Santa before sunrise!\n")
+time.sleep(3)
 
 response = input("Will you save Santa and stop Krampus from ruining Christmas? (Y/N)")
 
