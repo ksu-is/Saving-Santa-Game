4c406f1192e63fe2aae77e5d75e8891599a44e04
AlaysiahHernton <142437051+AlaysiahHernton@users.noreply.github.com>
2023-11-20 17:42:52 -0500

    fixed capitalization in script

--- saving-santa.py
+++ saving-santa.py
@@ -6,7 +6,7 @@ print("as the elves wrap the final presents and load up Santa's sleigh.\n")
 print("Suddenly, a loud crash echoes through the silent night!")
 print("The elves gasp as the workshop door flies open with a bang, ")
 print("a blast of cold air rushing in.")
-print("Standing in the doorway is the shadowy figure of krampus, ")
+print("Standing in the doorway is the shadowy figure of Krampus, ")
 print("the evil christmas monster!")
 print("His eyes glow red as he lets out a menacing laugh. \n")
 
