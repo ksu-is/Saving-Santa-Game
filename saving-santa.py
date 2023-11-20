508e07a37fc7651fd5d097b94c66751778d673ae
AlaysiahHernton <142437051+AlaysiahHernton@users.noreply.github.com>
2023-11-20 17:20:42 -0500

    Update saving-santa.py
    Added some lines for the opening script

---
+++ saving-santa.py
@@ -0,0 +1,4 @@
+print("It's the night before chrismas and all through the North Pole")
+print("the elves are hard at work, busy in Santa's workshop.")
+print("Stockings are hung by chimneys with care,")
+print("as the elves wrap the final presents and load up Santa's sleigh.\n")
