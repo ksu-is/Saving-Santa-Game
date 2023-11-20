b94b926be9164299898352b112c4cc12df1ad2a5
AlaysiahHernton <142437051+AlaysiahHernton@users.noreply.github.com>
2023-11-20 17:30:43 -0500

    Update saving-santa.py
    added more lines of opening script

--- saving-santa.py
+++ saving-santa.py
@@ -2,3 +2,12 @@ print("It's the night before chrismas and all through the North Pole")
 print("the elves are hard at work, busy in Santa's workshop.")
 print("Stockings are hung by chimneys with care,")
 print("as the elves wrap the final presents and load up Santa's sleigh.\n")
+
+print("Suddenly, a loud crash echoes through the silent night!")
+print("The elves gasp as the workshop door flies open with a bang, ")
+print("a blast of cold air rushing in.")
+print("Standing in the doorway is the shadowy figure of krampus, ")
+print("the evil christmas monster!")
+print("His eyes glow red as he lets out a menacing laugh. \n")
+
+print('"Ho ho ho!" mocks Krampus. "You didn\'t think Christmas would go on without a hitch, did you?"\n')
